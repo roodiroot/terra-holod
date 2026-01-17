8fbb36126fbfb44f6ded2634010f3f9006c972a9
Maxim <maxim@MacBook-Air-Maxim.local>
2026-01-17 23:08:13 +0300

    fix:1

--- components/overlay/widgets/constanse.ts
+++ components/overlay/widgets/constanse.ts
@@ -6,5 +6,5 @@ export type SOCIALType = {
 export const link_list: SOCIALType[] = [
   { icon: "ws", bg: "#25D366", link: process.env.NEXT_PUBLIC_WHATSAPP_LINK },
   { icon: "tg", bg: "#0088cc", link: process.env.NEXT_PUBLIC_TG_LINK },
-  { icon: "vk", bg: "#0077FF", link: process.env.NEXT_PUBLIC_VK_LINK },
+  // { icon: "vk", bg: "#0077FF", link: process.env.NEXT_PUBLIC_VK_LINK },
 ];
